67e5cf6ff02b10acc61f20febcf5d7520f000601
VidakovicDominik <domividakovic@gmail.com>
2019-10-08 19:57:02 +0200

    player and minefield foundation

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    private Tile[,] minefield;
+    public GameObject tilePrefab;
+    public int sizeX=10;
+    public int sizeY=10;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    void Start()
+    {
+        init();
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void init()
+    {
+        minefield = new Tile[sizeX, sizeY];
+        for(int i = 0; i < sizeX; i++)
+        {
+            for(int j = 0; j < sizeY; j++)
+            {
+                minefield[i, j] = Instantiate(tilePrefab, new Vector3(i, 0, j), Quaternion.identity).GetComponent<Tile>();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public TileType tyleType=TileType.EMPTY;
+
+    public bool triggered=false;
+
+    public Material triggeredMaterial;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!triggered && other.gameObject.CompareTag("Player"))
+            TriggerTile();
+    }
+
+    public void TriggerTile()
+    {
+        triggered = true;
+        this.GetComponent<Renderer>().material = triggeredMaterial;
+        transform.localScale = new Vector3(transform.localScale.x, 0.01f, transform.localScale.z);
+    }
+
+}
+
+public enum TileType
+{
+    MINE,CLEARED,EMPTY,HINT
+}
